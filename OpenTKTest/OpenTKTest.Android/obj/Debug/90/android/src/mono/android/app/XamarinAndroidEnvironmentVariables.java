ed66547ddcf9a4cb4310c3dd5f0f316d83308490
Cesar18102 <cesar18102000@gmail.com>
2019-06-28 17:30:54 +0300

    drawArrays drawElements

--- OpenTKTest/OpenTKTest.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ OpenTKTest/OpenTKTest.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "5472b592-b777-4540-be1d-4ec031438283",
+		"XAMARIN_BUILD_ID", "85c1e492-0c31-4476-8d9f-6ce6ccdbafe7",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
--- OpenTKTest/OpenTKTest/MainPage.xaml.cs
+++ OpenTKTest/OpenTKTest/MainPage.xaml.cs
@@ -6,10 +6,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-using OpenTK;
+/*using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.ES30;
-using OpenTK.Platform;
+using OpenTK.Platform;*/
 
 using Android.Opengl;
 using Android.Views;
@@ -24,10 +24,18 @@ namespace OpenTKTest
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-        private int program = 0;
-        private float dx = 0;
+        int rotateSide = 1;
 
-        private int[] buffers = new int[1];
+        private Random Rand = new Random();
+
+        private int program = 0;
+        private float scale = 1;
+        private float red = 1;
+
+        private int[] VAO = new int[1];
+        private int[] VBO = new int[1];
+        private int[] IBO = new int[1];
+
         private static float[] vertices = {
             0f, 0f, 0f,
             0.95f, 0f, 0f,
@@ -38,9 +46,28 @@ namespace OpenTKTest
             0.95f, -0.95f, 0f
         };
 
+        private static byte[] inds = {
+            3, 4, 5,
+            4, 5, 6
+        };
+
+        private static float[] mat = {
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        };
+
+        private static Matrix world = Matrix.FromArray<float>(mat) as Matrix;
+
         private J.Buffer buffer = J.ByteBuffer.AllocateDirect(vertices.Length * sizeof(float)).
                                                Order(J.ByteOrder.NativeOrder()).AsFloatBuffer().
                                                Put(vertices).Position(0);
+
+        private J.Buffer ibuffer = J.ByteBuffer.AllocateDirect(inds.Length * sizeof(int)).
+                                                Order(J.ByteOrder.NativeOrder()).
+                                                Put(inds).Position(0);
+
 
         public MainPage()
         {
@@ -59,21 +86,59 @@ namespace OpenTKTest
             {
                 program = PreloadShaders();
 
-                GLES32.GlGenBuffers(1, buffers, 0);
-                GLES32.GlBindBuffer(GLES32.GlArrayBuffer, buffers[0]);
+                GLES32.GlEnable(GLES32.GlVertexArray);
+
+                GLES32.GlGenVertexArrays(1, VAO, 0);
+                GLES32.GlBindVertexArray(VAO[0]);
+
+                GLES32.GlGenBuffers(1, VBO, 0);
+                GLES32.GlBindBuffer(GLES32.GlArrayBuffer, VBO[0]);
                 GLES32.GlBufferData(GLES32.GlArrayBuffer, vertices.Length * sizeof(float), buffer, GLES32.GlStaticDraw);
+
+                int posloc = GLES32.GlGetAttribLocation(program, "Position");
+                GLES32.GlVertexAttribPointer(posloc, 3, GLES32.GlFloat, false, 0, 0);
+                GLES32.GlEnableVertexAttribArray(posloc);
+
+                GLES32.GlGenBuffers(1, IBO, 0);
+                GLES32.GlBindBuffer(GLES32.GlElementArrayBuffer, IBO[0]);
+                GLES32.GlBufferData(GLES32.GlElementArrayBuffer, inds.Length * sizeof(byte), ibuffer, GLES32.GlStaticDraw);
+
+                GLES32.GlBindBuffer(GLES32.GlElementArrayBuffer, 0);
                 GLES32.GlBindBuffer(GLES32.GlArrayBuffer, 0);
+                GLES32.GlBindVertexArray(0);
+
+                GLES32.GlUseProgram(program);
             }
 
             GLES32.GlClearColor(0, 0, 0, 1);
             GLES32.GlClear(GLES32.GlColorBufferBit | GLES32.GlDepthBufferBit);
 
             GLES32.GlViewport(0, 0, (int)(GLView.Width * 3), (int)(GLView.Height * 3));
-            GLES32.GlUseProgram(program);
 
-            GLES32.GlEnableVertexAttribArray(0);
-            GLES32.GlVertexAttribPointer(0, 3, GLES32.GlFloat, false, 0, buffer);
-            GLES32.GlDrawArrays(GLES32.GlLineStrip, 0, 7);
+            GLES32.GlLineWidth(50);
+
+            int scaleLocation = GLES32.GlGetUniformLocation(program, "gScale");
+            //scale = (scale + 0.005f) % 1;
+            GLES32.GlUniform1f(scaleLocation, scale);
+
+            /*int colorLocation = GLES32.GlGetUniformLocation(program, "color");
+            red = (float)((red + 0.005f) % (2.0 * Math.PI));
+            GLES32.GlUniform4f(colorLocation, (float)Math.Abs(Math.Sin(red)), 0, 0, 1);*/
+
+            int worldLocation = GLES32.GlGetUniformLocation(program, "world");
+
+            //Matrix.TranslateM(mat, 0, 0f, 0f, 0f);
+            Matrix.RotateM(mat, 0, 1 * rotateSide, 0, 0, 1);
+            //Matrix.TranslateM(mat, 0, 0f, 0f, 0f);
+
+            GLES32.GlUniformMatrix4fv(worldLocation, 1, true, mat, 0);
+
+            GLES32.GlBindVertexArray(VAO[0]);
+            GLES32.GlBindBuffer(GLES32.GlElementArrayBuffer, IBO[0]);
+            GLES32.GlDrawElements(GLES32.GlTriangles, 6, GLES32.GlUnsignedByte, 0);
+
+            //GLES32.GlBindBuffer(GLES32.GlArrayBuffer, VBO[0]);
+            //GLES32.GlDrawArrays(GLES32.GlTriangleFan, 0, 7);
 
             GLES32.GlFinish();
         }
@@ -90,11 +155,25 @@ namespace OpenTKTest
 
         private int PreloadShaders()
         {
-            string vertexShaderSrc = "attribute vec4 vPosition;\n" +
-                                     "void main() { gl_Position = vPosition; }";
+            string vertexShaderSrc = "precision highp float;\n" +
+                                     "precision highp int;\n" +
+
+                                     "varying vec4 Color;\n" +
+                                     "uniform float gScale;\n" +
+                                     "uniform mat4 world;\n" +
+
+                                     "attribute vec4 Position;\n" +
+
+                                     "void main() { gl_Position = vec4(gScale * Position.x, gScale * Position.y, Position.z, 1.0) * world; \n" +
+                                                   "Color = clamp(Position, 0.2, 0.8); }";
 
             string fragmentShaderSrc = "precision mediump float;\n" +
-                                       "void main() { gl_FragColor = vec4 (1.0, 1.0, 1.0, 1.0); }";
+                                       "precision mediump int;\n" +
+
+                                       "varying vec4 Color;\n" +
+                                       //"uniform vec4 color;\n" +
+
+                                       "void main() { gl_FragColor = Color; }";
 
             int vertexShader = LoadShader(GLES32.GlVertexShader, vertexShaderSrc);
             int fragmentShader = LoadShader(GLES32.GlFragmentShader, fragmentShaderSrc);
@@ -104,10 +183,16 @@ namespace OpenTKTest
             GLES32.GlAttachShader(program, vertexShader);
             GLES32.GlAttachShader(program, fragmentShader);
 
-            GLES32.GlBindAttribLocation(program, 0, "vPosition");
+            GLES32.GlBindAttribLocation(program, 0, "Position");
             GLES32.GlLinkProgram(program);
 
             return program;
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            rotateSide = width > height ? 1 : -1;
         }
     }
 }
